b946c802a7cb5d7acc67d79752dfd5007329ea19
Thomas Platts <67462990+TPlatts04@users.noreply.github.com>
2024-08-17 21:49:15 +0100

    Base files

--- finance_manager.py
+++ finance_manager.py
@@ -1,4 +1,11 @@
-from finance_classes import *
 import os
 import csv
 import sys
+
+if __name__ == "__main__":
+    if os.path.isfile(FILENAME):
+        main()
+    else:
+        with open(FILENAME, "w") as file:
+            file.writelines("name,typeOfSpend,amount\n")
+        main()
